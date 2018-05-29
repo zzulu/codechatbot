06269aa9c3a03f5c5f6fe38375e39b4bae60dbed
Junwoo Hwang <capollux10@gmail.com>
2018-05-29 22:48:33 +0900

    Fix wrong syntax

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,7 @@
 class ApplicationController < ActionController::Base
   rescue_from CanCan::AccessDenied do |e|
     respond_to do |format|
-      format.json { message: { text: '권한이 없습니다.' } }
+      format.json { render json: {'message':{'text':'권한이 없습니다.'}}, status: :unauthorized }
       format.html { redirect_to bots_path }
     end
   end
