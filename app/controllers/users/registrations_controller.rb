1c9946881acc6f2f38c2e28717bee4b32a93d5ab
Junwoo Hwang <capollux10@gmail.com>
2018-06-01 10:59:48 +0900

    Users have to type their current_password when update info(rollback)

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -66,8 +66,4 @@ class Users::RegistrationsController < Devise::RegistrationsController
   # def after_inactive_sign_up_path_for(resource)
   #   super(resource)
   # end
-
-  def update_resource(resource, params)
-    resource.update_without_password(params)
-  end
 end
