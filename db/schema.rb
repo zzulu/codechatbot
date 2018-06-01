e546a0806d97629dce6c5b5117ce0c056707aad5
Junwoo Hwang <capollux10@gmail.com>
2018-06-01 11:10:40 +0900

    Add migration for reset_password_token

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_05_20_080155) do
+ActiveRecord::Schema.define(version: 2018_06_01_020537) do
 
   create_table "bots", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.bigint "user_id"
@@ -35,8 +35,6 @@ ActiveRecord::Schema.define(version: 2018_05_20_080155) do
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "username", default: "", null: false
     t.string "encrypted_password", default: "", null: false
-    t.string "reset_password_token"
-    t.datetime "reset_password_sent_at"
     t.integer "sign_in_count", default: 0, null: false
     t.datetime "current_sign_in_at"
     t.datetime "last_sign_in_at"
@@ -51,7 +49,10 @@ ActiveRecord::Schema.define(version: 2018_05_20_080155) do
     t.string "connection_code"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
     t.index ["connection_code"], name: "index_users_on_connection_code", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
     t.index ["username"], name: "index_users_on_username", unique: true
   end
 
